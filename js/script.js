02c3383ed6487627bdd3821eb0a848cb21e14f0b
hannah-w25 <77864662+hannah-w25@users.noreply.github.com>
2021-04-04 20:36:51 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,49 +1,24 @@
-var articleTitles = {
-  txGovr: 'Potential Opponents for Governor Abbott in His 2022 Bid for Re-Election',
-  hurricaneSeason: 'Climate Scientists Offer Their Projections on the Gulf\'s Hurricans Season',
-  energyInd: 'The Prospect of Increased Fossil Fuel Regulations is Raising Concerns about Houston\'s Economy',
-  txLegis: 'An Overview of This Year\'s Texas Legislative Agenda',
-  txSchools: 'The Percentage of K-12 Students With Failing Grades Has Risen Sharply During the Pandemic',
-  workplaces: 'These Houston Area Companies are Hiring Right Now',
-  eduCost: 'Analysis: Is Higher Education Becoming More of an Expense than an Investment?',
-  investmentTips: 'Stocks to Watch in 2021',
-  nextflixDocs: 'The Best Documentaries on Netflix Right Now',
-  covidCelebs: 'Remembering the Celebrities That Passed Away Due to COVID-19',
-  covidDelays: 'Pandemic Production Delays: Check to See When Your Favorites are Expected to Return',
-  bookRecs: 'This Month \'s Book Recommendation From the Editorial Board',
-  coffee: 'Your Caffeine Crash and the Science Behind It',
-  dogsIndoors: 'Ways to Keep Your Dog Entertained Indoors',
-  appsMindfulness: 'Apps for Practicing Mindfulness',
-  familyCooking: 'Fun Ways to Get Your Kids Interested in Cooking',
-  capitolInv: 'New Information Released on the Capitol Riot',
-  bidenAdminCovid: 'The Biden Administration\'s Coronavirus Strategy So Far',
-  genz2020: 'Coming of Age in an Era of Chaos: How Will 2020 & 2021 Impact GenZ?',
-  gopFuture: 'The Post-Trump Future of the GOP',
-  vaxEff: 'The Efficacy of the COVID Vaccines Against New Strains',
-  astronEvents: 'An Overview of Recent Rare Events in Astronomy and Upcoming Events',
-  eaCovid: 'East Asia Sees a New Surgence in Covid Cases',
-  fpRussia: 'Cybersecurity and the Future of U.S. Foreign Policy'
-};
-var imgBooks = $('<img id="bookshelf-img">');
-var img2020 = $('<img id="illustration-2020-img">');
-var imgObservatory = $('<img id="observatory-img">');
-var imgBidenHarris = $('<img id="biden-and-harris-img">');
-var imgCeleb = $('<img id="larry-king-img">');
-var imgCoffee = $('<img id="coffee-illustration-img">');
-var imgCovidEA = $('<img id="covid-asia-train-img">');
-var imgCovidSchools = $('<img id="empty-classroom-img">');
-var imgDogActivities = $('<img id="woman-with-dog-img">');
-var imgEduCost = $('<img id="edu-cost-img">');
-var imgRefinery = $('<img id="refinery-img">');
-var imgFamilyCooking = $('<img id="baking-with-kid-img">');
-var imgTrump = $('<img id="trump-img">');
-var imgHurricane = $('<img id="hurricane-sign-img">');
-var imgStocks = $('<img id="stock-graph-img">');
-var imgMedApp = $('<img id="minfulness-app-img">');
-var imgNetflix = $('<img id="tv-netlfix-img">');
-var imgCongressDarkBg = $('<img id="congress-dark-bg-img">');
-var imgRussiaBinary = $('<img id="russia-binary-illustration-img">');
-var imgTXGovr = $('<img id="greg-abbott-img">');
-var imgTXCapitol = $('<img id="tx-capitol-bldg-img">');
-var imgVax = $('<img id="vax-img">');
-var imgWorkplaces = $('<img id="meetingroom-img">');
+$(document).ready(function(){
+  var navCheckbox = $('#checkbox-nav');
+  var saveCheckbox = $('.article-save') || $('.saveT');
+  const isChecked = this.checked;
+	$(saveCheckbox).change(function save(){
+    let uncheckedIcon = 'https://uploads-ssl.webflow.com/6059475c6bc6f10900dfad0b/605948e14e751c281b184d76_bookmark-thin.svg';
+    let checkedIcon = 'https://uploads-ssl.webflow.com/6059475c6bc6f10900dfad0b/605948e12564f58466cea791_bookmark-thin-orange.svg';
+    let iconElem = this.nextElementSibling;
+    var bookmark = (isChecked == true) ? checkedIcon : uncheckedIcon;
+ 	  $(iconElem).attr('src',bookmark);
+  });
+  $('.onb-checkbox').change(function box0() {
+    let wrapperBox = this.parent();
+    let addActive = wrapperBox.addClass('active0');
+    let removeActive = wrapperBox.removeClass('active0');
+    let style = (isChecked == true) ? addActive : removeClass;
+    return style;
+  });
+  $(navCheckbox).change(function openNav(){
+    let desktopNav = $('#menu-dropdown');
+    let visibility = (isChecked == true) ? show() : hide();
+    $(desktopNav).visibility;
+  };
+});
